2d975866128d99f959bb9e2a88da53274051ece6
dili <muhammaddilia99@gmail.com>
2026-07-16 15:57:59 +0700

    feat:finalize portfolio

--- src/store/location.js
+++ src/store/location.js
@@ -5,8 +5,9 @@ const DEFAULT_LOCATION = locations.work;
 const useLocationStore = create(
   immer((set) => ({
     activeLocation: DEFAULT_LOCATION,
-    setActiveLocation: (location = null) =>
+    setActiveLocation: (location) =>
       set((state) => {
+        if (location === undefined) return;
         state.activeLocation = location;
       }),
 
